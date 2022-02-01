2c79b6a68d7e4b2ea4d3febee42ddabe8f0ff80c
Thomas F Steeples <thomas.f.steeples@gmail.com>
2022-02-01 12:08:50 +0000

    Added paper

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ function main()
     mkdir "${SCRIPT_DIR}"/public
     mkdir "${SCRIPT_DIR}"/public/css
     mkdir "${SCRIPT_DIR}"/public/js
-
+    
     cp "${SCRIPT_DIR}"/robots.txt "${SCRIPT_DIR}"/public
 
     cp -r "${SCRIPT_DIR}"/html/* "${SCRIPT_DIR}"/public
@@ -21,9 +21,9 @@ function main()
     cp -r "${SCRIPT_DIR}"/media "${SCRIPT_DIR}"/public
     cp -r "${SCRIPT_DIR}"/fonts "${SCRIPT_DIR}"/public
 
-    cp -r "${SCRIPT_DIR}"/aamas_presentation "${SCRIPT_DIR}"/public
-    cp -r "${SCRIPT_DIR}"/computational_game_theory "${SCRIPT_DIR}"/public
     cp -r "${SCRIPT_DIR}"/papers "${SCRIPT_DIR}"/public
+
+    cp -r "${SCRIPT_DIR}"/confirmation-presentation "${SCRIPT_DIR}"/public
 }
 
 main
